aac1fe8367bcb02f997bd2619860a65b70cc0a36
Unknown <icbjayasinghe@gmail.com>
2017-08-23 21:14:55 +0530

    Revert "loading same page(kela una thena)"
    This reverts commit 4b19241412dce0ad0e110d297788f04a4d4d0bb4.

--- Dashbord/import_manager/new_requision.php
+++ Dashbord/import_manager/new_requision.php
@@ -18,7 +18,6 @@
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="../../css/skins/_all-skins.min.css">
-  <link rel="stylesheet" href="../../js/navigation_controler.js">
 
     <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
@@ -168,16 +167,15 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        <li  id="dd"class="active treeview menu-open">
-          <a href="#">
+        <li class="treeview">
+          <a href="dashboard.php">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
            
           </a>
          
         </li>
        
-        <li class="treeview">
-                <li class="treeview">
+        <li class="treeview menu-open active">
           <a href="#">
             <i class="fa fa-edit"></i> <span>Purchase Requisitions</span>
             <span class="pull-right-container">
@@ -185,8 +183,8 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="newrequisition" name="newrequisition" class="fa fa-circle-o"></i> New Requisition</a></li>
-            <li><a href="pendingrequesition" name="pendingrequisition"><i class="fa fa-circle-o"></i> Pending Requisition</a></li>
+            <li><a href="#"><i class="fa fa-circle-o" style="color: aliceblue;"></i><span style="color: aliceblue;"> New Requisition</span> 
+            <li><a href="pending_requision.php"><i class="fa fa-circle-o"></i> Pending Requisition</a></li>
            </ul>
         </li>
         <li class="treeview">
@@ -233,6 +231,117 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    <h1>New Requision</h1>
+   
+      <ol class="breadcrumb">
+        <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#"><i class="fa"></i> Purchase Requisition</a></li>
+        <li class="active">New Requisition</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <div class="container">
+      <div class="box">
+        </div>
+        <div class="row">
+          <strong class="col-xs-2">Tire Brand</strong>
+          <div class="col-xs-2">
+            <select class="form-control" >
+            <option value="">Select</option>
+             <option value="" >Dunlop</option>
+             <option value="" >Kaizen</option>
+           </select>
+          </div>
+          
+          <strong class="col-xs-2">Country</strong>
+        
+          <div class="col-xs-2">
+          <select class="form-control" >
+             <option value="" >Select</option>
+             <option value="" >Japan</option>
+			  <option value="">Thaiwan</option>
+          </select>
+          </div>
+          <button class="col-xs-1 btn btn-success" type="button">Go</button>
+          </div>
+          <div class="row">
+            <div class="col-xs-12" style="width: auto; margin-left: 72px">
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Available Tires</h3>
+                </div>
+                <div class="box-body">
+                  <table id="tire_ava" class="table-bordered table-hover" width="920">
+                    <thead>
+                      <tr>
+                        <th>Tire ID</th>
+                        <th>Size</th>
+                        <th>Available Qty</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <td>dj1801</td>
+                      <td>18</td>
+                      <td>4</td>
+                      <td>checked</td>
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.box-body -->
+              </div>
+              <!-- /.box -->
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12" style="width: auto; margin-left: 72px">
+              <div class="box">
+                <div class="box-header">
+                <h3 class="box-title">Requisition Item</h3></div>
+              
+              <!-- /.box-header -->
+              <div class="box-body">
+                <table id="Requisition_itm_tbl" class="table-bordered table-hover" width="920">
+                <thead>
+                  <tr>
+                  	<th>Select</th>
+                    <th>Tire ID</th>
+                    <th>Size</th>
+                    <th>Required Qty</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  	<td><input type="checkbox" name="select_tire"></td>
+                    <td>dj1802</td>
+                    <td>18</td>
+                    <td><input type="name" name="qty"></td>
+                  </tr>
+                </tbody>
+                </table>
+              </div>
+              <!-- /.box-body -->
+              </div>
+              <!-- /.box -->
+            </div>
+          </div>
+          <div class="additempanal">
+       <div class="row" style="background-color:#D2D2D2; margin-left:70px; margin-right: 130px; ">
+       </br><div class="col-xs-3">
+
+       <button type="button" class="btn btn-success" style="width: 160px; margin-left: 20px">Send Selected</button></div>
+       
+    <div class="col-xs-3"><button type="button" class="btn btn-primary" style="width: 160px">Send All Items</button></div>
+  <div class="col-xs-3"><button type="button" class="btn btn-warning" style="width: 160px">Remove Selected Item</button></div>
+  <div class="col-xs-3"><button type="button" class="btn btn-danger" style="width: 160px">Remove All Items</button></div></br></br></br>
+    </div>
+    </div>
+
+    </div>
+
     
   </div>
   <!-- /.content-wrapper -->
