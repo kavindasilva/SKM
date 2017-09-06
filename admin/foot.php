06c27f37832bec5cc350b3b559121725bf7293e5
kavindasilva <kss.kavinda@gmail.com>
2017-09-06 20:10:21 +0530

    inclusion of single header and footer

---
+++ admin/foot.php
@@ -0,0 +1,58 @@
+<?php
+/**
+	this file contains the footer part belove the main content.
+	******This file should be include( or require) at the end of every .php file containing UI in the admin folder
+	head.php contains the header part, which is above the main content
+*/
+
+?>
+
+    <!-- end of Main content -->
+    
+  </div>
+  <!-- /.content-wrapper -->
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      Sole Agent of Dunlop tires in Srilanka
+    </div>
+    <strong>S.K.Muanasinge Motors </strong> 
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+  
+  </aside>
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+
+<!-- jQuery 3.1.1 -->
+<script src="../js/jquery-3.1.1.min.js"></script>
+<!-- Bootstrap no need 3.3.7 -->
+<script src="../js/bootstrap.min.js"></script>
+<!-- FastClick no need -->
+<script src="../js/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../js/demo.js"></script>
+<script src="../js/navigation_controler.js?2"></script>
+
+</body>
+
+</html>
+
+<?php
+
+
+?>
