108c050d6bfe7b43974e38f52e14e2df8bcd5a37
Unknown <icbjayasinghe@gmail.com>
2017-12-05 12:31:09 +0530

    dulminage bala kirima matha push

--- Dashbord/import_manager/quary.php
+++ Dashbord/import_manager/quary.php
@@ -7,6 +7,7 @@
  */
 require_once('../../php/dbcon.php');
 $tire_id=$_POST['tire_id'];
+$tire_qty=$_POST['qty'];
 
 //getting suplier id for insert purchase requisition table
 $sup_id_quary="select supplier_s_id from tire WHERE t_id=$tire_id";
@@ -14,8 +15,15 @@ $sup_id_result=mysqli_query($conn,$sup_id_quary);
 
 while ($row=mysqli_fetch_array($sup_id_result)){
     $sup_id=$row['supplier_s_id'];
+    //inserting purchase_requisition table
     $pr_tbl_quary="INSERT INTO `purchase_requisition` (`pr_no`, `date`, `status`, `supplier_s_id`, `import_manager_employee_e_id`) VALUES (NULL, CURDATE(), 'pending', $sup_id, '2')";
     mysqli_query($conn,$pr_tbl_quary);
-    echo"$sup_id";
+
+    $pr_no_quary="SELECT MAX(pr_no) FROM purchase_requisition";
+    $pr_no=mysqli_fetch_row(mysqli_query($conn,$pr_no_quary))[0];
+
+
+    $pr_item_tbl_quary="INSERT INTO `pr_item` (`tire_t_id`, `purchase_requisition_pr_no`, `qty`) VALUES ($tire_id,$pr_no,$tire_qty)";
+    echo"$pr_no";
 }
 ?>
