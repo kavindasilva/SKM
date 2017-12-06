a175a675cce00483f454b75f696ba81857898c44
Unknown <icbjayasinghe@gmail.com>
2017-12-06 10:58:47 +0530

    work with pending requisition

--- Dashbord/import_manager/pending_requision.php
+++ Dashbord/import_manager/pending_requision.php
@@ -34,26 +34,69 @@
             <th>Brand</th>
             <th>Country</th>
             <th>Size</th>
+            <th>Requested qty</th>
             <th>Sent Date</th>
           </tr>
         </thead>
-        <tbody>
-          <td>dj1802</td>
-          <td>Dunlop</td>
-          <td>Japan</td>
-          <td>18X26</td>
-          <td>05.05.1994</td>
+        <tbody id="pending_item">
+        <?php
+        require_once('../../php/dbcon.php');
+        $tire_tbl_quary="SELECT t_id,brand_name,country,tire_size,quantity,status FROM tire WHERE status='pending'";
+        $result = $conn->query($tire_tbl_quary);
+        while($row=$result->fetch_assoc()){
+            //$tbl_rw_id=$tbl_rw_id+1;
+            $t_id=$row['t_id'];
+            ?>
+
+            <tr class="clickable-row" id="ava_tire_row">
+
+                <td><?php echo $row['t_id']?></td>
+                <td><?php echo $row['brand_name']?></td>
+                <td><?php echo $row['country']?></td>
+                <td><?php echo $row['tire_size']?></td>
+
+            <?php
+        }
+        //requested quantity retrive
+        $pr_itm_tbl_quary="SELECT qty,purchase_requisition_pr_no FROM pr_item WHERE tire_t_id=$t_id";
+        $qty_exe=mysqli_query($conn,$pr_itm_tbl_quary);
+        $qty_row=mysqli_fetch_row($qty_exe);
+
+        ?>
+        <td><?php echo $qty_row[0]?></td>
+        <?php
+
+        $pr_no=$qty_row[1];
+        $pr_tbl_quary="SELECT date FROM purchase_requisition WHERE pr_no=$pr_no";
+        $date_exe=mysqli_query($conn,$pr_tbl_quary);
+        $date_row=mysqli_fetch_row($date_exe);
+
+        $conn->close();
+        ?>
+        <td><?php echo $date_row[0]?></td>
+        <td><button class="btn btn-danger delete_btn">Delete Requests</button></td>
+        </tr>
+
         </tbody>
       </table>
       
     </div>
     </div> 
   </div>
+<script>
+    $(".delete_btn").click(function () {
+        
+
+        
+    })
+</script>
+
 
  
 
 
 </body>
+
 
 
 </html>
