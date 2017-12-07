817367687dc9912085ef92e375def4aee8e969f7
kavindasilva <kss.kavinda@gmail.com>
2017-12-07 19:06:55 +0530

    reports3

---
+++ Dashbord/cheif_manager/report3.php
@@ -0,0 +1,69 @@
+
+<?php
+/**
+	This is the chief managers normal reports.
+	Monthly sold items quantity
+*/
+require_once "../../php/dbcon.php";
+
+$date=$_POST['months'];
+$year=$_POST[''];
+$month=$_POST[''];
+
+$strSQL = "Select * From tire;";
+//$strSQL="SELECT sum(o.qty) FROM tire t, sales_order s, order_item o where o.tire_t_id=t.t_id and o.sord_no=s.sord_no and o.status='available'";
+
+//$strSQL="SELECT t.t_id, t.brand_name,t.country,t.tire_size,sum(o.qty) as totq FROM tire t, sales_order s, order_item o where o.tire_t_id=t.t_id and o.sord_no=s.sord_no and o.status='available' GROUP by t.t_id";
+
+$strSQL="SELECT t.t_id, t.brand_name,t.country,t.tire_size,sum(o.qty) as totq FROM tire t, sales_order s, order_item o where o.tire_t_id=t.t_id and o.sord_no=s.sord_no and o.status='available' and year(s.date)='2017' and month(s.date)='10'  GROUP by t.t_id";
+
+$objQuery = mysqli_query($conn,$strSQL) or die ("Database query failed: $strSQL<hr>" . mysqli_error($conn)); //result set
+
+if(mysqli_num_rows($objQuery)==0){
+	echo "No data in system";
+}
+else{
+	echo "<table>";
+	echo "<tr> <th>Tire id</th> <th>Brand</th> <th>Country</th>"; //<th>type</th>
+	echo " <th>Size</th>";// <th>unit price(Rs.)</th> 
+	echo "<th>Quantity</th> </tr>";
+	while ($eachResult=mysqli_fetch_assoc($objQuery)) 
+	{ //width
+		/*$this->Cell(10);
+		if($eachResult["quantity"]<=20){
+			$this->SetFillColor(200,100,100);
+		}*/
+		//$this->Cell(10,6,$tmpcnt,1);
+		echo "<tr>";
+		echo "<td>".$eachResult["t_id"]."</td>";
+		echo "<td>".$eachResult["brand_name"]."</td>";
+		echo "<td>".$eachResult["country"]."</td>";
+		
+		//echo "<td>".$eachResult["t_type"]."</td>";
+		echo "<td>".$eachResult["tire_size"]."</td>";
+		//echo "<td>".$eachResult["unit_price"]."</td>";
+		
+		echo "<td>".$eachResult["totq"]."</td>";
+	 	 	 	
+		echo "</tr>"; 	
+	}
+	echo "</table>";
+}
+
+
+//echo "file called";
+?>
+
+<script type="text/javascript">
+	function openTab(url){
+		window.open(url, '_blank');
+	}
+</script>
+
+
+
+
+
+
+
+
--- Dashbord/cheif_manager/reportm.php
+++ Dashbord/cheif_manager/reportm.php
@@ -11,7 +11,7 @@ echo "file called";
 
 ?>
 
-<script type="text/javascript">
+<script type="text/javascript" src="chiefmgr.js">
 	function openTab(url){
 		window.open(url, '_blank');
 	}
@@ -21,7 +21,11 @@ echo "file called";
 <a href="report2.php" target="frm1"><input type="button" name="" value="Current stock status report"  class="btn btn-sm btn-info"> </a>
 <input type="button" name="" value="Current stock status report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports2.php')" /> 	<br/>
 
-<input type="button" name="" value="Monthly stock status report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports2.php')" /> 	<br/>
+<form>
+	<input type="month" name="months" id="months" onchange="checkCur(this.id)"/>
+	<a href="report3.php" id="monthsbtn" target="frm1"><input type="button" name="" value="Monthly sales report"  class="btn btn-sm btn-info" disabled/> </a>
+	<input type="button" id="monthsbtn" name="" value="Monthly sales report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports3.php')" /> 	<br/>
+</form>
 
 <input type="button" name="" value="Monthly stock status report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports2.php')" /> 	<br/>
 
