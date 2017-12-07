c349cf26478e5a028bfa0b7d86c164349f9b4fc6
kavindasilva <kss.kavinda@gmail.com>
2017-12-07 19:30:42 +0530

    mothly sales stock report. passing parameters not working

--- Dashbord/cheif_manager/reportm.php
+++ Dashbord/cheif_manager/reportm.php
@@ -10,8 +10,13 @@ echo "file called";
 
 
 ?>
+<html>
+	
+<body>
 
-<script type="text/javascript" src="chiefmgr.js">
+<script type="text/javascript" src="chiefmgr.js"></script>
+
+<script type="text/javascript" >
 	function openTab(url){
 		window.open(url, '_blank');
 	}
@@ -21,11 +26,11 @@ echo "file called";
 <a href="report2.php" target="frm1"><input type="button" name="" value="Current stock status report"  class="btn btn-sm btn-info"> </a>
 <input type="button" name="" value="Current stock status report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports2.php')" /> 	<br/>
 
-<form>
-	<input type="month" name="months" id="months" onchange="checkCur(this.id)"/>
-	<a href="report3.php" id="monthsbtn" target="frm1"><input type="button" name="" value="Monthly sales report"  class="btn btn-sm btn-info" disabled/> </a>
-	<input type="button" id="monthsbtn" name="" value="Monthly sales report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports3.php')" /> 	<br/>
-</form>
+
+	<input type="month" name="months" id="months" onchange="checkCur('months')"/>
+	<a href="report3.php" target="frm1"><input type="button" id="monthsbtn" name="" onclick="testAlert()" value="Monthly sales report"  class="btn btn-sm btn-info" /> </a>
+	<input type="button" id="monthsbtn2" name="" value="Monthly sales report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports3.php')" /> 	<br/>
+
 
 <input type="button" name="" value="Monthly stock status report"  class="btn btn-sm btn-info" onclick="openTab('../../report/reports2.php')" /> 	<br/>
 
@@ -37,7 +42,8 @@ echo "file called";
 <iframe src="" name="frm1" id="frm1" width=800 height=500 style="" >
 </iframe>
 
-
+</body>
+</html>
 
 
 
