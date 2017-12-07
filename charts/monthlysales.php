8c502a2831bcd0a2396b3b9849c3bf29a6176f5c
Dula <rdulmina@gmail.com>
2017-12-07 18:14:46 +0530

    chart working

--- charts/monthlysales.php
+++ charts/monthlysales.php
@@ -1,10 +1,10 @@
 <?php
 // this file gives monthly sales of dunlop and kaizen tires
-require_once('../php/dbcon.php');
+require_once('../../php/dbcon.php');
 function getreport($brandtype){
 echo "[";	
-for($month_given=8;$month_given<=12;$month_given++)	{
-$GLOBALS['tcount']=0;
+for($month_given=6;$month_given<=12;$month_given++)	{
+$tcount=0;
 	
 $getinvoce="SELECT * FROM sales_order";
 if($result=mysqli_query($GLOBALS['conn'],$getinvoce)){
@@ -12,18 +12,18 @@ if($result=mysqli_query($GLOBALS['conn'],$getinvoce)){
 		$date=$order['date'];
 		$date=explode("-",$date);
 		$month=$date[1];
-		echo $month;
+		//echo $month;
 		if($month_given==$month){//check the month is matchin to given month
 			//then get the order items of that order
 			$order_items="SELECT * FROM order_item where sord_no=\"".$order['sord_no']."\" and status='Issued';";
-			if($result=mysqli_query($GLOBALS['conn'],$order_items)){
-				while($order_item=mysqli_fetch_array($result)){//taking each order item
+			if($result2=mysqli_query($GLOBALS['conn'],$order_items)){
+				while($order_item=mysqli_fetch_array($result2)){//taking each order item
 					$tid=$order_item['tire_t_id'];
 					$gettire="SELECT brand_name FROM tire WHERE t_id=$tid";
-					$result=mysqli_query($GLOBALS['conn'],$gettire);	
-					$tire=mysqli_fetch_array($result);
+					$result3=mysqli_query($GLOBALS['conn'],$gettire);	
+					$tire=mysqli_fetch_array($result3);
 					if($tire['brand_name']==$brandtype){
-						$GLOBALS['tcount']+=$order_item['qty'];
+						$tcount+=$order_item['qty'];
 					}
 					
 					
@@ -32,10 +32,10 @@ if($result=mysqli_query($GLOBALS['conn'],$getinvoce)){
 		}
 	}
 if($month_given==12){
-	echo $GLOBALS['tcount']."]";
+	echo $tcount."]";
 }
 else{
-	echo $GLOBALS['tcount'].",";
+	echo $tcount.",";
 }	
 
 }
@@ -43,6 +43,6 @@ else
 	echo mysqli_error($conn);	
 }
 }
-getreport("Dunlop");
+
 
 ?>
