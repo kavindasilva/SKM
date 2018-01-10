c9817161c3a12592f828efa748aa3df82cdad01a
Dula <rdulmina@gmail.com>
2018-01-10 15:41:29 +0530

    history page

---
+++ Dashbord/Stock_Manager/history.php
@@ -0,0 +1,51 @@
+<html>
+ <link href="../../css/aos.css" rel="stylesheet">
+  <script src="../../js/plugins.js"></script>
+  <section class="content-header">
+   <h1>
+        History
+   </h1>     
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+         <li><a href="#"><i class="fa"></i> Stock</a></li>
+          <li class="active"><a href="#"><i class="fa"></i> Manage Stock</a></li>
+          <li class="active"><a href="#"><i class="fa"></i> History</a></li>
+      </ol>
+   </section>
+    
+<div class="box">   		
+    	</div>   
+<section class="content">
+	<div  class="col-xs-12 col-md-12" >
+       <div class="box">
+           <div class="box-body" style="overflow-x:auto;" data-aos="zoom-in-right">
+    <table class="table table-hover" id="stocktable">
+	<thead>
+		<tr>
+			<th>Country</th>
+			<th>Brand name</th>
+			<th>Tyre Size</th>
+            <th>Qty</th>
+            <th>Unit Price</th>
+            <th>Status</th>
+            <th>Tyre Type</th>
+            <th> </th>
+		</tr>
+	</thead>
+	<tbody>
+		
+	</tbody>
+	</table>
+			  </div>
+		</div>
+	</div>
+</section>   
+   
+   
+   
+   
+   
+</html>   
+<script>
+    AOS.init();
+</script> 
