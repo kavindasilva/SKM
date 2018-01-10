16722c02fcb65655698a8ac83afce1be1a228ca5
Dula <rdulmina@gmail.com>
2018-01-10 21:17:43 +0530

    spelling correction

--- Dashbord/Stock_Manager/dashbord.php
+++ Dashbord/Stock_Manager/dashbord.php
@@ -1,6 +1,6 @@
   <div style="padding: 7px;">
   <section class="content-header">
-   <h1>Dashbord</h1>
+   <h1>Dashboard</h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         
