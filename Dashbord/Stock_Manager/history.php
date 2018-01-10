8128833e66cf2615c81b731dd841828c0fbed851
kavindasilva <kss.kavinda@gmail.com>
2018-01-10 22:08:14 +0530

    restore(not working)

--- Dashbord/Stock_Manager/history.php
+++ Dashbord/Stock_Manager/history.php
@@ -3,9 +3,52 @@
 	require_once('../../php/dbcon.php');
 ?>
 
+<?php
+	//restore code
+	if(isset($_POST['resto'])){
+		$coun=$_POST['country'];
+		$brr=$_POST['brand_name'];
+		$TID=$_POST['ttid'];
+			
+		//$_POST['sdate']." @ ".$row['stime'];
+		//echo "<td>".$tttime."</td>"; //time
+		$ts=$_POST['tire_size'];
+		
+		$qqq=$_POST['qty'];
+		$upp=$_POST['unitprice'];
+		$stt=$_POST['status'];
+		$typ=$_POST['type'];
+		
+		$sql4="update tire set country='$coun', brand_name='$brr', quantity=$qqq, unit_price=$upp, status='$stt', t_type='$typ' where t_id=".$TID;
+		
+		if(!mysqli_query($conn, $sql4)){
+			echo mysqli_error($conn);
+			return;
+		}
+		else{
+			echo "<script>alert('Data restored successfully');</script>";
+			echo "<script>window.location.href='index.php';</script>";
+			
+		}
+		
+		
+	}
+?>
+
 <html>
  <link href="../../css/aos.css" rel="stylesheet">
   <script src="../../js/plugins.js"></script>
+  
+  <script type="text/javascript">
+  	function confirmR() {
+	var x = confirm("Are you sure you want to Restore these details?");
+	if (x)
+		return true;
+	else
+		return false;
+}
+  </script>
+  
   <section class="content-header">
    <h1>
         History
@@ -61,17 +104,29 @@
 		}
 		
 		while($row=mysqli_fetch_array($res)){
-			echo "<tr><td>".$row['country']."</td>";
-			echo "<td>".$row['brand_name']."</td>";
+			echo "<form name='abc' method='post' action='history2.php'>";
+			
+			echo "<input type='text' value='".$row['tireid']."' name='ttid' hidden>";
+			
+			echo "<tr><td>".$row['country']."</td>"; echo "<input type='text' name='country' value='".$row['country']."' hidden>";
+			echo "<td>".$row['brand_name']."</td>"; echo "<input type='text' name='brand_name' value='".$row['brand_name']."' hidden>";
 			
 			$tttime=$row['sdate']." @ ".$row['stime'];
-			echo "<td>".$tttime."</td>"; //time
-			echo "<td>".$row['tire_size']."</td>";
+			echo "<td>".$tttime."</td>"; //time 
+			//echo "<input type='text' name='tireid' value='".$row['tireid']."' hidden>";
 			
-			echo "<td>".$row['qty']."</td>";
-			echo "<td>".$row['unitprice']."</td>";
-			echo "<td>".$row['status']."</td>";
-			echo "<td>".$row['type']."</td></tr>";
+			echo "<td>".$row['tire_size']."</td>"; echo "<input type='text' name='tire_size' value='".$row['tire_size']."' hidden>";
+			
+			echo "<td>".$row['qty']."</td>"; echo "<input type='text' name='qty' value='".$row['qty']."' hidden>";
+			echo "<td>".$row['unitprice']."</td>"; echo "<input type='text' name='unitprice' value='".$row['unitprice']."' hidden>";
+			echo "<td>".$row['status']."</td>"; echo "<input type='text' name='status' value='".$row['status']."' hidden>";
+			echo "<td>".$row['type']."</td>"; echo "<input type='text' name='type' value='".$row['type']."' hidden>";
+			
+			echo "<td>";
+			echo "<input name='resto' type='submit' value='Restore' onclick='return confirmR();'>";
+			echo "</td></tr>";
+			
+			echo "</form>";
 		}
 		
 		
@@ -88,6 +143,8 @@
    
    
 </html>   
+
+
 <script>
     AOS.init();
 </script> 
